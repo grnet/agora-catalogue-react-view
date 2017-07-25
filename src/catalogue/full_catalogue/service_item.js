c8f84514261f1565fd51dcfdbd8460bbc33b32a8
Thanasis Katsadas <kathan@grnet.gr>
2017-07-25 12:00:46 +0300

    Render new fields
    Render a list of user tags, based on user_customers_list key. Also
    render short_description on the all services list page. Last, update of
    the api endpoint to use the development server of agora,
    agora-dev.vi-seem.eu

--- src/catalogue/full_catalogue/service_item.js
+++ src/catalogue/full_catalogue/service_item.js
@@ -8,12 +8,26 @@ class ServiceItem extends React.Component {
     return `https://sp.eudat.eu/static/img/logos/${this.props.data.logo}`;
   }
 
+  renderHtml(markup) {
+    return {__html: markup};
+  }
+
+  renderUserTags() {
+    let tags = [];
+    tags = this.props.data.user_customers_list.user_customers.map((item) => {
+      return <div className="user-tag">{ item.name }</div>
+    });
+    return tags;
+  }
+
   render() {
     return (
       <div className="service-item">
         <ServiceLogo img_name={this.props.data.logo} />
         <h3>{ this.props.data.name }</h3>
         <div className="service-area">Service Area: { this.props.data.service_area }</div>
+        <p className="short-description" dangerouslySetInnerHTML={this.renderHtml(this.props.data.short_description)} />
+        { (this.props.data.user_customers_list.count > 0) ? this.renderUserTags() : '' }
         <Link to={`/catalogue/${this.props.data.service.name}`}>Read more </Link>
       </div>
     );
